28805c776a95f17202c4732461fd2b2946a51f5e
ukkiy-ray <showy13f207ag@gmail.com>
2020-10-21 12:35:52 +0900

    passwordを英数字混合にするためのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,7 @@ class User < ApplicationRecord
     validates :last_name_furigana
     validates :first_name_furigana
   end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
