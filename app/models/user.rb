71e06e92a506e31bf9adc151894d5a6e537835e6
ukkiy-ray <showy13f207ag@gmail.com>
2020-10-21 12:09:20 +0900

    名前とフリガナ入力におけるバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_furigana, presence: true
-  validates :first_name_furigana, presence: true
   validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字で入力してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナで入力してください' } do
+    validates :last_name_furigana
+    validates :first_name_furigana
+  end
 end
