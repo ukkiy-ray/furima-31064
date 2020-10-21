f22d74af9eca0ca293023f5058cf5dcc002a6c83
ukkiy-ray <showy13f207ag@gmail.com>
2020-10-21 19:24:15 +0900

    指摘のあったコードの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length:6)
-    password {password}
+    password {'abcd1234'}
     password_confirmation {password}
     last_name           {'山田'}
     first_name          {'太郎'}
