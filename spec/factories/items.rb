453426ce1177a86dbd2c7afe0d4823ace15697dd
ukkiy-ray <showy13f207ag@gmail.com>
2020-10-22 21:24:43 +0900

    細かいコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -9,7 +9,7 @@ FactoryBot.define do
     delivery_charge_id {'2'}
     delivery_area_id {'2'}
     delivery_period_id {'2'}
-    price {'1_000_000'}
+    price {'1000000'}
     
     after(:build) do |content|
       content.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
