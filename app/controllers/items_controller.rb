690a3326e53e2e25821a4be21830e520a9c8c583
ukkiy-ray <showy13f207ag@gmail.com>
2020-10-22 12:36:37 +0900

    active hashを利用したカテゴライズ等の機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :item_details, :status_id, :category_id, :delivery_charge_id, :delivery_area_id, :days_to_delivery_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :delivery_area
+  belongs_to_active_hash :days_to_delivery
+
+  belongs_to :user
+  has_one_attached :image
+
+  #空の投稿を保存できないようにする
+  validates :title, :item_details, :status_id, :category_id, :delivery_charge_id, :delivery_area_id, :days_to_delivery_id, :price, :image, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :delivery_charge_id, numericality: { other_than: 1 }
+  validates :delivery_area_id, numericality: { other_than: 1 }
+  validates :days_to_delivery_id, numericality: { other_than: 1 }
 end
